a0aaad1b169b69dadb946754abc5062026c0903d
Ignacio Sanchez <sanchezih@gmail.com>
2022-09-15 11:11:09 -0300

    add profiles spring

--- src/main/java/com/github/sanchezih/ownblog/Application.java
+++ src/main/java/com/github/sanchezih/ownblog/Application.java
@@ -1,12 +1,25 @@
 package com.github.sanchezih.ownblog;
 
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 
 @SpringBootApplication
 public class Application {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -14,7 +27,22 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+	}
+
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+		final Environment env = event.getApplicationContext().getEnvironment();
+
+		LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+
+//		final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+//
+//		StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource)
+//				.map(ps -> ((EnumerablePropertySource) ps).getPropertyNames()).flatMap(Arrays::stream).distinct()
+//				.filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
+//				.forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
 	}
 
 }
